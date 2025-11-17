ca6d32cbde6d032c332a9bda84bcc103cf5e2cf4
sumitkumawat090912-dot <sumitkumawat090912@gmail.com>
2025-11-17 17:50:38 +0530

    Update OWNER and user IDs in vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -7,14 +7,14 @@ API_ID = int(environ.get("API_ID", "29899250"))
 API_HASH = environ.get("API_HASH", "611d045796c79af3e5ddfa3d6fd536a7")
 BOT_TOKEN = environ.get("BOT_TOKEN", "8461876221:AAHPI9rFngzd875t67t-54PLn_VXOaksHFw")
 
-OWNER = int(environ.get("OWNER", "8167927496"))
+OWNER = int(environ.get("OWNER", "7549194607"))
 CREDIT = environ.get("CREDIT", "@ll_kumawat_ll")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '8167927496').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '7549194607').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '8167927496').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7549194607').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
